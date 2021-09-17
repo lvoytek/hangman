21a8a3a7b175c77207bd448ef65d92ecec5d7d46
Lena Voytek <lena@ieee.org>
2021-09-16 21:50:18 -0700

    Add functions for extracting dictionary and choosing words

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Hangman
+
 The game hangman written in Go, because why not
+
+Default dictionary words.txt is taken from [dwyl/english-words](https://github.com/dwyl/english-words/blob/master/words.txt)
--- hangman.go
+++ hangman.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"os"
+	"regexp"
 	"strings"
+	"time"
 )
 
 func main() {
 	stages := ExtractGallows("default.gallows")
 
 	fmt.Println(stages[5])
+
+	currentWord := ChooseWord(ExtractDictionary("words.txt"))
+	fmt.Println(currentWord)
 }
 
 // ExtractGallows returns a set of stages that the gallows and body can be in
@@ -39,4 +46,49 @@ func ExtractGallows(gallowsFile string) []string {
 	gallowsStages = append(gallowsStages, fileString)
 
 	return gallowsStages
+}
+
+// ChooseWord picks a random word from a string array and returns it.
+// If there are no strings in the list, return an empty string
+func ChooseWord(wordList []string) string {
+	if len(wordList) == 0 {
+		return ""
+	}
+
+	s1 := rand.NewSource(time.Now().UnixNano())
+	timeBasedRand := rand.New(s1)
+
+	return wordList[timeBasedRand.Intn(len(wordList))]
+}
+
+// ExtractDictionary reads from a dictionary file and return a string
+// array containing strings with only lowercase letters log an error
+// and returns nil if the file is invalid
+// A dictionary in this case is a text file containing words separated by
+// '\n' new lines.
+func ExtractDictionary(dictionaryFile string) []string {
+	file, err := os.Open(dictionaryFile)
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return nil
+	}
+
+	dictScanner := bufio.NewScanner(file)
+	dictScanner.Split(bufio.ScanLines)
+
+	isLowercaseWord := regexp.MustCompile(`^[a-z]+$`).MatchString
+
+	var outputWords []string
+	var currentWord string
+
+	for dictScanner.Scan() {
+		currentWord = dictScanner.Text()
+
+		if isLowercaseWord(currentWord) {
+			outputWords = append(outputWords, currentWord)
+		}
+	}
+
+	return outputWords
 }
